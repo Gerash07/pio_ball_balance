a3cd45d338bf024c1ffac8d0319ff58d68a43e2e
Sofya <towel.fm@gmail.com>
2023-12-26 20:29:06 +0300

    Обратная связь

--- src/student.cpp
+++ src/student.cpp
@@ -15,6 +15,7 @@ void controllerInit (Overlord &over)
  * @brief Выполнить одну итерацию системы управления
  * @details Вызывается раз в 5мс
  */
+float w = 0;
 void controllerTick (Overlord &over)
 {
     float setPoint = -over.getSetpoint ();
@@ -24,8 +25,18 @@ void controllerTick (Overlord &over)
     float motorVel = over.getMotorVel ();
 
     bool button = !digitalRead(13);
-    float u = button * over.getSlider(SliderEnum::prog1)/ 1000 * 1.0;
+    float w0 = button * over.getSlider(SliderEnum::prog1)* 1.0 / 1000 ;
 
+    float K = 0.68;
+    float Kp = 8;
+
+    float err = w0 - motorVel;
+
+    float u = Kp * err;
+
+
+    Serial.print(w0);
+    Serial.print(' ');
     Serial.print(u);
     Serial.print(' ');
     Serial.println(motorVel);
